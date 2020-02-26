de880c39d853ba33c30f19265579e757cfc921ae
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2020-02-26 22:02:24 +0800

    Delete go.sh

--- go.sh
+++
@@ -1,6 +0,0 @@
-bash TLUpdate.sh
-mv TLUpdate.sh /usr/lib/
-echo "[+]Move script to /usr/lib/ successfully !"
-(crontab -l && echo "0 4 */2 * * bash /usr/lib/TLUpdate.sh") | crontab
-echo "[+]Add script to crontab successfully, Please Remove this git floder !"
-
