20a65bc39a3d2bbd1a05848fc57d1463bba57872
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2020-03-15 18:08:19 +0800

    Update TLUpdate.sh

--- TLUpdate.sh
+++ TLUpdate.sh
@@ -5,6 +5,7 @@ FloderName=${TmpStr##*/}
 FileName='trackers_all.txt'
 FullName="/tmp/${FloderName}/${FileName}"
 #Test aria2
+aria2_survival(){
 aria2c --version
 if [ $? == 0 ];then
 	echo "[+]Aria2 have installed"
@@ -14,7 +15,9 @@ else
 	apt install aria2 -y
 	echo '[-]Have installed Aira2'
 fi
+}
 #Aria2 floder
+aria2_folder(){
 if [ -d $HOME/.aria2 ]; then 
 	echo "[+].aria2 folder is exited"
 else
@@ -22,7 +25,9 @@ else
 	mkdir $HOME/.aria2
 	echo "[-]Have created directory .aria2"
 fi
+}
 #Test TrackersList Floder
+TrackersList_Folder(){
 echo "[+]Check TrackerList floder....."
 cd /tmp
 if [ -d /tmp/${FloderName} ];then
@@ -32,7 +37,9 @@ if [ -d /tmp/${FloderName} ];then
 else
 	echo "[+]The floder(git) is not existed!"
 fi
+}
 #Check for git
+git_check(){
 git --version
 if [ $? == 0 ];then
 	echo "[+]git have installed"
@@ -42,7 +49,9 @@ else
 	echo '[-]Install git successfully'
 fi
 echo "[+]Download TrackersList...."
+}
 #Download txt Source File
+Download_Source(){
 git clone ${SourceURL}
 echo "[+]Download success!"
 #Find old Output File
@@ -52,17 +61,24 @@ if [ -f /tmp/aria2.conf ]; then
 else
 	echo "[+]/tmp/aria2.conf is not exist"
 fi
+}
+
 #Generate output
+Generate_output(){
 RealOut=$(echo $(grep . ${FullName}) | sed 's/ /,/g')
 echo "bt-tracker=${RealOut}" > /tmp/aria2.conf
+}
 #Aria2 old File Test
+old_file{
 if [ -f $HOME/.aria2/aria2.conf ]; then 
 	echo "[+]$HOME/.aria2/aria2.conf  is exited"
 	rm $HOME/.aria2/aria2.conf
 else
 	echo "[-]$HOME/.aria2/aria2.conf  is not exited"
 fi
+}
 #Aria2 new File movement
+Move_new_file(){
 echo "[+]Move aria2.conf file"
 mv /tmp/aria2.conf $HOME/.aria2/aria2.conf
 echo "[+]Move the file successfully"
@@ -71,3 +87,12 @@ rm -r /tmp/${FloderName}
 echo "[+]Clean up the rubbish file successfully!"
 cd ~
 exit
+}
+aria2_survival
+aria2_folder
+TrackersList_Folder
+git_check
+Download_Source
+Generate_output
+old_file
+Move_new_file
