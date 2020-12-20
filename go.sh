dc7d0f3330e428ea5d4c993373c18e4a20d429db
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2020-12-20 23:00:14 +0800

    Add Execute permission

--- go.sh
+++ go.sh
@@ -1,4 +1,5 @@
+chmod u+x ~/aria2c_TrackersList/TLUpdate.sh
 bash ~/aria2c_TrackersList/TLUpdate.sh
 cp ~/aria2c_TrackersList/TLUpdate.sh /usr/lib/ && echo "[+]Move script to /usr/lib/ successfully !" || echo "[-]Move is failed"
-((crontab -l ; echo "0 4 */2 * * bash /usr/lib/TLUpdate.sh" ) | crontab ) && echo "[+]Add script to crontab successfully, It has removed the git floder successfully !!" || echo "[-]Add script is failed"
+((crontab -l ; echo "0 4 */2 * * /usr/lib/TLUpdate.sh" ) | crontab ) && echo "[+]Add script to crontab successfully, It has removed the git floder successfully !!" || echo "[-]Add script is failed"
 rm -r ~/aria2c_TrackersList
