54170b1edd8a324fbde7d3f24527edad2b369568
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2020-12-20 22:58:50 +0800

    Justice SELinux context type

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 bash ~/aria2c_TrackersList/TLUpdate.sh
-mv ~/aria2c_TrackersList/TLUpdate.sh /usr/lib/ && echo "[+]Move script to /usr/lib/ successfully !" || echo "[-]Move is failed"
+cp ~/aria2c_TrackersList/TLUpdate.sh /usr/lib/ && echo "[+]Move script to /usr/lib/ successfully !" || echo "[-]Move is failed"
 ((crontab -l ; echo "0 4 */2 * * bash /usr/lib/TLUpdate.sh" ) | crontab ) && echo "[+]Add script to crontab successfully, It has removed the git floder successfully !!" || echo "[-]Add script is failed"
 rm -r ~/aria2c_TrackersList
